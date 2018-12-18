413381ef4e783fe6a5138d9a0a5bea386ac79467
PhilipHoddinott <philiphoddinott@gmail.com>
2018-12-17 20:14:23 -0500

    The End
    Undergrad over last year.
    This is the last for my gradschool.
    
    It's been a ride.
    
    For any one reading this is in the future good luck!

--- designOpNN/latexCode/testAcc.m
+++ designOpNN/latexCode/testAcc.m
@@ -1,32 +1,30 @@
-function [numCorrect, numErrors,acc] = testAcc(pNet, inputValues, labels, sizeArr)
+function [numCorrect, numErrors,acc] = testAcc(pNet, inputValuesTest, labelsTes, sizeArr)
     % testAcc test the accuracy of a net using mnist validation set
     %
     % INPUT:
     % pNet : net
-    % inputValues : MNIST Input values for training
-    % labels : MNIST Labels for validation
+    % inputValuesTest : MNIST Input values for testing
+    % labelsTest : MNIST Labels for validating testing
     % sizeArr : net architecture
     %
     % OUTPUT:
     % numCorrect : number of correctly classified numbers.
     % numErrors : number of classification errors.
+    % acc : test accuracy
     % 
-    testSetSize = size(inputValues, 2);
-    numErrors = 0;   numCorrect = 0;
-    
-    for n = 1: testSetSize
-        inputVector = inputValues(:, n);        
-        outputVector = pNet.netOutput(inputVector,sizeArr);
-        maxV = 0; class = 1;
+    numErrors = 0;   numCorrect = 0; % set to zero
+    for n = 1: size(inputValuesTest, 2)
+        outputVector = pNet.netOutput(inputValuesTest(:, n),sizeArr);
+        maxVal = 0; classVal = 1;
         
         for i = 1: size(outputVector, 1)
-            if outputVector(i) > maxV
-                maxV = outputVector(i); % get maxV
-                class = i; % get class
+            if outputVector(i) > maxVal
+                maxVal = outputVector(i); % get maxV
+                classVal = i; % get class
             end
         end
         
-        if class == labels(n) + 1
+        if classVal == labelsTes(n) + 1
             numCorrect = numCorrect + 1; % count correct
         else
             numErrors = numErrors + 1; % count error
